81c846a7044509cada0bf253af04706bdc311886
anggeralmasih <angger.almasih@gmail.com>
2022-01-23 00:47:27 +0700

    (Praktikum 11) = Modul 6 (Hosting)

---
+++ Pertemuan 10/example-app/lav/resources/views/beranda.blade.php
@@ -0,0 +1,7 @@
+@extends('template.base')
+@section('content')
+    <h1>Beranda</h1>
+    <br>
+    <h5>Halo, nama saya Anggeralmasih Wiradika Rusmana, biasa dipanggil Angger.</h5>
+    <p>Saya mahasiswa Intitut Teknologi Telkom Purwokerto jurusan S1 Rekayasa Perangkat Lunak.</p>
+@endsection
---
+++ Pertemuan 10/example-app/lav/resources/views/template/base.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="//cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
+    <title>Latihan Laravel</title>
+</head>
+
+<body>
+    <div class="container mt-5" style="height: 500px">
+        <div class="row">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a href="#" class="navbar-brand">Laravel</a>
+                    <button class="navbar-toggler" type="button" data-toggler="collapse"
+                        data-target="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a href="{{ route('index') }}" class="nav-link active">Beranda <span class="sr-only">(current)</span></a>
+                            <a href="{{ route('student.index') }}" class="nav-link">Mahasiswa</a>
+                            {{-- <a href="{{ route('student.data') }}" class="nav-link">Data Tables</a> --}}
+                            <a href="{{ route('about') }}" class="nav-link">About</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mb-5">
+        <footer class="text-white text-center pb-2 pt-4 mt-5 bg-dark">
+            <p>Improved by <a
+                    href="https://www.instagram.com/angger.almasih/" class="text-white" style="font-weight: bold">Angger Almasih</a>
+            </p>
+        </footer>
+    </div>
+
+
+
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    {{-- Datatables Script --}}
+    <script src="//cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready( function () {
+            $('#myTable').DataTable();
+        } );
+    </script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
